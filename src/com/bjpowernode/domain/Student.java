267b96039f854fe10503e985af4ecc11211a5ab1
wq <wq@bjpowernode.com>
2020-07-29 16:00:34 +0800

    asdasd

---
+++ src/com/bjpowernode/domain/Student.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;
+
+/**
+ * @USER: 王小q1an
+ * @DATE: 2020/7/29
+ */
+public class Student {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+}
